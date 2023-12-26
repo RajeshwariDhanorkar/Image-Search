acb6c10031d9ac3d61fa986856edfffbf3e7f20d
shwari <dhanorkarrajeshwari@gmail.com>
2023-12-26 17:26:10 +0530

    dom project

---
+++ script.js
@@ -0,0 +1,65 @@
+const accesskey ="-gysqB545uLIg4PZy6TDXcjk6RwwNNQK97LSoB_RjOs";
+const formE1 = document.querySelector("form");
+const input  = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+
+
+let inputData = " "
+let page = 1;
+
+
+async function searchimages(){
+    inputData = input.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`
+
+    const response = await fetch(url);
+
+    const  data = await response.json();
+
+    const results = data.results;
+
+
+    if (page === 1){
+        searchResults.innerHTML = ""
+
+    }
+
+    results.map((result) =>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textConetent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+
+    });
+
+
+    page++;
+    if (page > 1){
+        showMore.style.display = "block";
+    }
+
+
+}
+
+formE1.addEventListener("submit",(event) => {
+    event.preventDefault();
+    page=1;
+    searchimages();
+});
+
+showMore.addEventListener("click", () =>{
+    searchimages();
+
+});
+    
